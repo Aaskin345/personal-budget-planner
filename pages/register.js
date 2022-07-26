cffd2f29442139d559835c10e36389c79ee92123
Daren-Wallace <darenwalace@gmail>
2022-07-27 01:26:01 +0300

    Create Register Screen

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,12 @@
 import Link from 'next/link';
-//import React from 'react';
 import React, { useEffect } from 'react';
 import { signIn, useSession } from 'next-auth/react';
-
 import { useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
 import { getError } from '../utils/error';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function LoginScreen() {
   const { data: session } = useSession();
@@ -24,10 +23,17 @@ export default function LoginScreen() {
   const {
     handleSubmit,
     register,
+    getValues,
     formState: { errors },
   } = useForm();
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post('/api/auth/signup', {
+        name,
+        email,
+        password,
+      });
+
       const result = await signIn('credentials', {
         redirect: false,
         email,
@@ -41,13 +47,31 @@ export default function LoginScreen() {
     }
   };
   return (
-    <Layout title="Login">
+    <Layout title="Create Account">
       <div className="flex justify-center pt-20">
         <form
           className="max-w-screen-sm bg-white shadow-2xl rounded-3xl px-8 pt-6 pb-8 mb-4"
           onSubmit={handleSubmit(submitHandler)}
         >
-          <h1 className="mb-4 text-2xl font-bold text-sky-600">Login</h1>
+          <h1 className="mb-4 text-2xl font-bold text-sky-600">
+            Create Account
+          </h1>
+          <div className="mb-4">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="w-full"
+              id="name"
+              autoFocus
+              {...register('name', {
+                required: 'Please enter name',
+              })}
+            />
+            {errors.name && (
+              <div className="text-red-500">{errors.name.message}</div>
+            )}
+          </div>
+
           <div className="mb-4">
             <label htmlFor="email">Email</label>
             <input
@@ -61,7 +85,6 @@ export default function LoginScreen() {
               })}
               className="w-full"
               id="email"
-              autoFocus
             ></input>
             {errors.email && (
               <div className="text-red-500">{errors.email.message}</div>
@@ -86,17 +109,41 @@ export default function LoginScreen() {
               <div className="text-red-500 ">{errors.password.message}</div>
             )}
           </div>
+          <div className="mb-4">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              className="w-full"
+              type="password"
+              id="confirmPassword"
+              {...register('confirmPassword', {
+                required: 'Please enter confirm password',
+                validate: (value) => value === getValues('password'),
+                minLength: {
+                  value: 6,
+                  message: 'confirm password is more than 5 chars',
+                },
+              })}
+            />
+            {errors.confirmPassword && (
+              <div className="text-red-500 ">
+                {errors.confirmPassword.message}
+              </div>
+            )}
+            {errors.confirmPassword &&
+              errors.confirmPassword.type === 'validate' && (
+                <div className="text-red-500 ">Password do not match</div>
+              )}
+          </div>
+
           <div className="mb-4 ">
             <button className="rounded bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 w-full">
-              Login
+              Register
             </button>
           </div>
           <div className="mb-4 ">
-            Don&apos;t have an account? &nbsp;
+            Already have an account? &nbsp;
             <span className="text-sky-600 hover:text-sky-800">
-              <Link href={`/register?redirect=${redirect || '/'}`}>
-                Register
-              </Link>
+              <Link href={`/login?redirect=${redirect || '/'}`}>Login</Link>
             </span>
           </div>
         </form>
